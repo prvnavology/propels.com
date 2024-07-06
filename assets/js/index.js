66e057e3ef58b9c05b6008e79c49cc8efc402685
prvnavology <praveen.avology@gmail.com>
2024-07-06 20:35:09 +0530

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,11 @@
-$(".menu-icon").click(function() {
+$(".menu-icon").click(function () {
     $("body").addClass("menuToggle");
 });
-$(".close-icon").click(function() {
+$(".close-icon").click(function () {
     $("body").removeClass("menuToggle");
 });
 
-$(window).scroll(function(){
+$(window).scroll(function () {
     if ($(window).scrollTop() >= 30) {
         $('header').addClass('fixed-header');
         $('header').addClass('visible-title');
@@ -16,34 +16,90 @@ $(window).scroll(function(){
     }
 });
 
-let count = document.querySelectorAll(".count")
+
+// Counter JS Start
+
+let count = document.querySelectorAll(".count");
+let span= document.createElement("SPAN");
+span.innerHTML="+";
+document.getElementsC("myList").appendChild(node);
+
 let arr = Array.from(count)
+arr.map(function (item) {
+    let startnumber = 0
 
+    function counterup() {
+        startnumber++
+        item.innerHTML = startnumber
 
+        if (startnumber == item.dataset.number) {
+            clearInterval(stop)
+        }
+    }
 
-arr.map(function(item){
-  let startnumber = 0
-
-  function counterup(){
-  startnumber++
-  item.innerHTML= startnumber
-   
-  if(startnumber == item.dataset.number){
-      clearInterval(stop)
-  }
-}
-
-let stop =setInterval(function(){
-  counterup()
-},50)
+    let stop = setInterval(function () {
+        counterup()
+    }, 60)
 
 })
+
+// Counter JS End
+
+
+// Slick Slider JS Start
 
 $('.banner-slider').slick({
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed:2000,
-  });
- 
+    autoplaySpeed: 2000,
+});
+
+// Slick Slider JS End
+
+
+
+
+
+//Cookies JS Start
+
+function SetCookie(cookieName, cookieValue, nDays) {
+    "use strict";
+    var today = new Date();
+    var expire = new Date();
+    if (nDays == null || nDays == 0) nDays = 1;
+    expire.setTime(today.getTime() + 3600000 * 24 * nDays);
+    document.cookie = cookieName + "=" + escape(cookieValue) + ";expires=" + expire.toGMTString() + "; path=/";
+    }
+    function ReadCookie(cookieName) {
+    "use strict";
+    var theCookie = " " + document.cookie;
+    var ind = theCookie.indexOf(" " + cookieName + "=");
+    if (ind == -1) ind = theCookie.indexOf(";" + cookieName + "=");
+    if (ind == -1 || cookieName == "") return "";
+    var ind1 = theCookie.indexOf(";", ind + 1);
+    if (ind1 == -1) ind1 = theCookie.length;
+    return unescape(theCookie.substring(ind + cookieName.length + 2, ind1));
+    }
+    function DeleteCookie(cookieName){
+    "use strict";
+    var today = new Date();
+    var expire = new Date() - 30;
+    expire.setTime(today.getTime() - 3600000 * 24 * 90);
+    document.cookie = cookieName + "=" + escape(cookieValue) + ";expires=" + expire.toGMTString();
+    }
+    function AcceptCookies() {
+    SetCookie('sgCookies', true, 30);
+    jQuery("#cookie-bar").hide();
+    }
+    $(document).ready(function (e) {  
+    var toTop = $("<div id='cookie-bar'><div id='contentcookie'><p>We use cookies and similar technologies to recognize your repeat visits and preferences, to measure the effectiveness of campaigns, and improve our websites. For settings and more information about cookies, view our <a href='#'>Cookie Policy</a>. By clicking “I accept” on this banner or using our site, you consent to the use of cookies.</p><button id='Cookie' tabindex=1 onclick='AcceptCookies();'>I accept </button><div class='clear'></div></div></div>").appendTo("body");
+    if (!ReadCookie("sgCookies")) { //If the cookie has not been set
+    jQuery("#cookie-bar").show();
+    } else {
+    jQuery("#cookie-bar").hide();
+    }
+    });
+
+    //Cookies JS Start 
