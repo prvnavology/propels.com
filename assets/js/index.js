876efb1227f7d35ca792bf8623dc6a74a4530363
prvnavology <praveen.avology@gmail.com>
2024-07-06 20:49:25 +0530

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,13 +16,9 @@ $(window).scroll(function () {
     }
 });
 
-
 // Counter JS Start
 
 let count = document.querySelectorAll(".count");
-let span= document.createElement("SPAN");
-span.innerHTML="+";
-document.getElementsC("myList").appendChild(node);
 
 let arr = Array.from(count)
 arr.map(function (item) {
@@ -36,7 +32,6 @@ arr.map(function (item) {
             clearInterval(stop)
         }
     }
-
     let stop = setInterval(function () {
         counterup()
     }, 60)
@@ -71,8 +66,8 @@ function SetCookie(cookieName, cookieValue, nDays) {
     if (nDays == null || nDays == 0) nDays = 1;
     expire.setTime(today.getTime() + 3600000 * 24 * nDays);
     document.cookie = cookieName + "=" + escape(cookieValue) + ";expires=" + expire.toGMTString() + "; path=/";
-    }
-    function ReadCookie(cookieName) {
+}
+function ReadCookie(cookieName) {
     "use strict";
     var theCookie = " " + document.cookie;
     var ind = theCookie.indexOf(" " + cookieName + "=");
@@ -81,25 +76,25 @@ function SetCookie(cookieName, cookieValue, nDays) {
     var ind1 = theCookie.indexOf(";", ind + 1);
     if (ind1 == -1) ind1 = theCookie.length;
     return unescape(theCookie.substring(ind + cookieName.length + 2, ind1));
-    }
-    function DeleteCookie(cookieName){
+}
+function DeleteCookie(cookieName) {
     "use strict";
     var today = new Date();
     var expire = new Date() - 30;
     expire.setTime(today.getTime() - 3600000 * 24 * 90);
     document.cookie = cookieName + "=" + escape(cookieValue) + ";expires=" + expire.toGMTString();
-    }
-    function AcceptCookies() {
+}
+function AcceptCookies() {
     SetCookie('sgCookies', true, 30);
     jQuery("#cookie-bar").hide();
-    }
-    $(document).ready(function (e) {  
+}
+$(document).ready(function (e) {
     var toTop = $("<div id='cookie-bar'><div id='contentcookie'><p>We use cookies and similar technologies to recognize your repeat visits and preferences, to measure the effectiveness of campaigns, and improve our websites. For settings and more information about cookies, view our <a href='#'>Cookie Policy</a>. By clicking “I accept” on this banner or using our site, you consent to the use of cookies.</p><button id='Cookie' tabindex=1 onclick='AcceptCookies();'>I accept </button><div class='clear'></div></div></div>").appendTo("body");
     if (!ReadCookie("sgCookies")) { //If the cookie has not been set
-    jQuery("#cookie-bar").show();
+        jQuery("#cookie-bar").show();
     } else {
-    jQuery("#cookie-bar").hide();
+        jQuery("#cookie-bar").hide();
     }
-    });
+});
 
-    //Cookies JS Start 
+//Cookies JS Start 
